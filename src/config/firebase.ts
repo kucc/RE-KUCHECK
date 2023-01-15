5087231156d9e201dfb08092d9efd7853fc731ff
LHJ <jjs01hwang@gmail.com>
2023-01-16 01:35:19 +0900

    FIX : firebase setting 변경

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -14,7 +14,6 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
 };
 
 export const app = initializeApp(firebaseConfig);
