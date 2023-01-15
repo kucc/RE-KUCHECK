0eab178584dad206b5e4e7456bf9115bb7a86dc9
yuna Kim <yuna000804@gmail.com>
2023-01-15 23:10:54 +0900

    FIX : 머지 문제 해결

---
+++ src/pages/ProfilePage/data.tsx
@@ -0,0 +1,40 @@
+export const userData = {
+  emoji: '👨‍🎤',
+  name: '정인아',
+  position: '운영진',
+  comment: '안녕하십니까 ~ 저는 정인아입니다요~! 여기는 한줄소개 칸이여유',
+  email: 'ina20118@korea.ac.kr',
+  github: 'qqq1212341',
+  instagram: 'huijun810',
+  detailComment: `안녕하십니까 ~ 저는 고려대학교 디자인조형학부 20학번 정인아입니다!\n지금 디자인하고 있는데 만족스럽습니다.\n열심히 만들었으니 많이 사용해주세요 히히`
+}
+
+export const mainCourseData = [
+  {
+    leader: '정인아',
+    leaderEmoji: '👨‍🎤',
+    title: '바닐라 자바스크립트 세션',
+    level: 'easy',
+    credit: '1학점',
+    total: '5',
+    registerd: '0', 
+  },
+  {
+    leader: '정인아',
+    leaderEmoji: '👨‍🎤',
+    title: '바닐라 자바스크립트 세션',
+    level: 'easy',
+    credit: '2학점',
+    total: '5',
+    registerd: '0', 
+  },
+  {
+    leader: '정인아',
+    leaderEmoji: '👨‍🎤',
+    title: '바닐라 자바스크립트 세션',
+    level: 'easy',
+    credit: '3학점',
+    total: '5',
+    registerd: '0', 
+  }
+]
