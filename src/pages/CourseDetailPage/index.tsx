521989dc886d2014f0c5315ca3a7deb309e4a236
LHJ <jjs01hwang@gmail.com>
2023-01-15 20:44:14 +0900

    FEATURE : courseDetailPage에 react-query 추가

--- src/pages/CourseDetailPage/index.tsx
+++ src/pages/CourseDetailPage/index.tsx
@@ -1,34 +1,52 @@
-import { StyledCommonLayout } from '@utility/COMMON_STYLE';
-import {
-        StyledContainer,
-        StyledTitle,
-        StyledLine,
-        StyledDesc,
-        StyledDescBox,
-        StyledDescTitle,
-        StyledDescDetail,
-        StyledEmoji,
-        StyledDetailContainer,
-        StyledDetailTitle,
-        StyledDetailTitleBase,
-        StyledDetailDesc,
-        StyledLine2,
-      } from './style';
-import { RED } from '@utility/COLORS';
-import { userInform, sessionInform, sessionCurriculum, sessionStack } from './data';
+import { useQuery } from '@tanstack/react-query';
+import { RouteComponentProps } from 'react-router';
 
-export const CourseDetailPage = () => {
-  // const courseId = props.match.params.id;
+import { getCourse } from '@apis';
+import { QUERY_KEY } from '@constants';
+import { RED } from '@utility/COLORS';
+import { StyledCommonLayout } from '@utility/COMMON_STYLE';
+
+import { sessionCurriculum, sessionInform, sessionStack, userInform } from './data';
+import {
+  StyledContainer,
+  StyledDesc,
+  StyledDescBox,
+  StyledDescDetail,
+  StyledDescTitle,
+  StyledDetailContainer,
+  StyledDetailDesc,
+  StyledDetailTitle,
+  StyledDetailTitleBase,
+  StyledEmoji,
+  StyledLine,
+  StyledLine2,
+  StyledTitle,
+} from './style';
+
+export const CourseDetailPage = ({ match }: RouteComponentProps<{ id: string }>) => {
+  const courseId = match.params.id;
+
+  // location.
+  const { isLoading, data } = useQuery({
+    queryFn: getCourse,
+    queryKey: [QUERY_KEY.course, courseId],
+  });
+
+  if (isLoading) return <div>로딩중...</div>;
+
+  console.log(data);
+
   return (
     <StyledCommonLayout>
-
       <StyledContainer>
         <StyledTitle>팀장</StyledTitle>
-        <StyledLine width="32px" />
-        <StyledDesc direction="row" style={{paddingTop: '5px', paddingBottom: '5px'}}>
+        <StyledLine width='32px' />
+        <StyledDesc direction='row' style={{ paddingTop: '5px', paddingBottom: '5px' }}>
           <StyledEmoji>{userInform.emoji}</StyledEmoji>
           <StyledDescBox>
-            <StyledDescTitle><span style={{fontWeight: 'bold'}}>{userInform.name}</span>님</StyledDescTitle>
+            <StyledDescTitle>
+              <span style={{ fontWeight: 'bold' }}>{userInform.name}</span>님
+            </StyledDescTitle>
             <StyledDescDetail fontSize='10px'>{userInform.desc}</StyledDescDetail>
           </StyledDescBox>
           <img src='/img/arrow.svg' />
@@ -37,40 +55,43 @@ export const CourseDetailPage = () => {
 
       <StyledContainer>
         <StyledTitle>사용 언어 및 기술 스택</StyledTitle>
-        <StyledLine width="119px" />
-        <StyledDesc direction="column">
+        <StyledLine width='119px' />
+        <StyledDesc direction='column'>
           {sessionStack.map((stack, i) => (
-              <>
-                <StyledDetailContainer gap='11px' key={i}>
-                  <StyledDetailTitle>{stack.title}</StyledDetailTitle>
-                  <StyledDetailDesc>
-                    {stack.desc.map((stackDetail, i) => (
-                      <div key={i}>- {stackDetail}</div>
-                    ))}
-                  </StyledDetailDesc>
-                </StyledDetailContainer>
-              </>
-            ))}
+            <>
+              <StyledDetailContainer gap='11px' key={i}>
+                <StyledDetailTitle>{stack.title}</StyledDetailTitle>
+                <StyledDetailDesc>
+                  {stack.desc.map((stackDetail, i) => (
+                    <div key={i}>- {stackDetail}</div>
+                  ))}
+                </StyledDetailDesc>
+              </StyledDetailContainer>
+            </>
+          ))}
         </StyledDesc>
       </StyledContainer>
 
       <StyledContainer>
         <StyledTitle>세션 소개</StyledTitle>
-        <StyledLine width="56px" />
-        <StyledDesc direction="column" style={{gap: '10px'}}>
+        <StyledLine width='56px' />
+        <StyledDesc direction='column' style={{ gap: '10px' }}>
           <div>
-            <StyledDescTitle style={{fontWeight: 'bold', marginBottom: '2px'}}>{sessionInform.course}</StyledDescTitle>
-            <StyledDescDetail fontSize='9px'>난이도: <span style={{color: `${RED}`}}>{sessionInform.level}</span> / 투자시간: <span style={{color: `${RED}`}}>{sessionInform.credit}</span></StyledDescDetail>
+            <StyledDescTitle style={{ fontWeight: 'bold', marginBottom: '2px' }}>
+              {sessionInform.course}
+            </StyledDescTitle>
+            <StyledDescDetail fontSize='9px'>
+              난이도: <span style={{ color: `${RED}` }}>{sessionInform.level}</span> / 투자시간:{' '}
+              <span style={{ color: `${RED}` }}>{sessionInform.credit}</span>
+            </StyledDescDetail>
           </div>
           <StyledLine2 />
-          <div style={{display: 'flex', flexDirection: 'column', gap: '15px'}}>
-            {sessionInform.detail.map((detail, i)=>(
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
+            {sessionInform.detail.map((detail, i) => (
               <>
                 <StyledDetailContainer gap='6px' key={i}>
                   <StyledDetailTitle>{detail.title}</StyledDetailTitle>
-                  <StyledDetailDesc>
-                    {detail.desc}
-                  </StyledDetailDesc>
+                  <StyledDetailDesc>{detail.desc}</StyledDetailDesc>
                 </StyledDetailContainer>
               </>
             ))}
@@ -80,24 +101,18 @@ export const CourseDetailPage = () => {
 
       <StyledContainer>
         <StyledTitle>커리큘럼</StyledTitle>
-        <StyledLine width="54px" />
-        <StyledDesc direction="column" style={{gap: '8px'}}>
+        <StyledLine width='54px' />
+        <StyledDesc direction='column' style={{ gap: '8px' }}>
           {sessionCurriculum.map((curri, i) => (
             <>
               <StyledDetailContainer gap='51px' key={i}>
                 <StyledDetailTitleBase>{curri.title}</StyledDetailTitleBase>
-                <StyledDetailDesc>
-                  {curri.desc}
-                </StyledDetailDesc>
+                <StyledDetailDesc>{curri.desc}</StyledDetailDesc>
               </StyledDetailContainer>
             </>
           ))}
         </StyledDesc>
       </StyledContainer>
-      
     </StyledCommonLayout>
   );
 };
-
-
-
