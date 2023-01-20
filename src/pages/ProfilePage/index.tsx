67f59ec0c8058b7d47a203e3de67833079f5a459
LHJ <jjs01hwang@gmail.com>
2023-01-21 02:54:06 +0900

    FIX : useEffect data fetch 수정

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
+
 import { useQuery } from '@tanstack/react-query';
 import { RouteComponentProps } from 'react-router';
 import { useHistory } from 'react-router-dom';
+
 import { getUser } from '@apis';
 import { QUERY_KEY } from '@constants';
 import {
@@ -13,6 +15,7 @@ import {
   StyledCourseDetail2,
   StyledCourseTitle,
 } from '@utility/COMMON_STYLE';
+
 import {
   StyledComment,
   StyledCourseContainer,
@@ -54,17 +57,21 @@ export const ProfilePage = ({ match }: RouteComponentProps<{ id: string }>) => {
   });
 
   const history = useHistory();
-  // course.semester === '23-1' 로 수정해야 됨
-  const courseNow = data?.courseHistory?.filter((course: Course) => course?.semester === '22-2');
-  const coursePast = data?.courseHistory?.filter((course: Course) => course?.semester !== '22-2');
 
-  const [courseSemester, setCourseSemester] = useState(courseNow);
-  const [courseTab, setCourseTab] = useState('now');
+  const [courseSemester, setCourseSemester] = useState<Course[] | null>(null);
+  const [courseTab, setCourseTab] = useState<'past' | 'now'>('now');
 
   useEffect(() => {
-    const courseNow = data?.courseHistory?.filter((course: Course) => course?.semester === '22-2');   
-    setCourseSemester(courseNow);
-  }, [data]);
+    if (!data) return;
+    const courseHistory = data.courseHistory ?? [];
+    const courseNow = courseHistory.filter((course: Course) => course.semester === '22-2');
+    const coursePast = courseHistory.filter((course: Course) => course.semester !== '22-2');
+    if (courseTab === 'now') {
+      setCourseSemester(courseNow);
+    } else if (courseTab === 'past') {
+      setCourseSemester(coursePast);
+    }
+  }, [data, courseTab]);
 
   if (isLoading) return <div>로딩중...</div>;
   if (isError) return <div>에러에요.</div>;
@@ -77,7 +84,6 @@ export const ProfilePage = ({ match }: RouteComponentProps<{ id: string }>) => {
     github_id = data.link.slice(github_id_index + 1);
   }
 
-  
   // useEffect(() => {
   //   // 임의 코스 데이터
   //   dispatch(getMainCourseRequest('21-2'));
@@ -99,111 +105,124 @@ export const ProfilePage = ({ match }: RouteComponentProps<{ id: string }>) => {
 
   return (
     <StyledCommonLayout>
-      <StyledBackArrowWrapper onClick={() => {history.goBack();}}>
+      <StyledBackArrowWrapper
+        onClick={() => {
+          history.goBack();
+        }}>
         <StyledBackArrow src={`${process.env.PUBLIC_URL}/img/common/backArrow.svg`} />
       </StyledBackArrowWrapper>
       <StyledCommonPcLayout>
-      <StyledUserInfoContainer>
-        <StyledUserEmoji>{data.emoji}</StyledUserEmoji>
-        <StyledUserContainer>
-          <StyledName>
-            {data.name}
-            <StyledUserRole>{data.role}</StyledUserRole>
-          </StyledName>
-          <StyledComment>{data.comment}</StyledComment>
-          <StyledSocialContainer>
-            <StyledSocialBox>
-              <img src={`${process.env.PUBLIC_URL}/img/social/email.svg`} />
-              <StyledSocialLink href={email} target='_blank'>
-                {data.email}
-              </StyledSocialLink>
-            </StyledSocialBox>
-            <StyledSocialBox>
-              <img src={`${process.env.PUBLIC_URL}/img/social/github.svg`} />
-              <StyledSocialLink href={data.link} target='_blank'>
-                {/* {data.link} */}
-                {github_id}
-              </StyledSocialLink>
-            </StyledSocialBox>
-            <StyledSocialBox>
-              <img src={`${process.env.PUBLIC_URL}/img/social/instagram.svg`} />
-              <StyledSocialLink href={instagram} target='_blank'>
-                @{data.instaLink}
-              </StyledSocialLink>
-            </StyledSocialBox>
-          </StyledSocialContainer>
-        </StyledUserContainer>
-        <StyledPcModifyButton>수정하기</StyledPcModifyButton>
-      </StyledUserInfoContainer>
-      <StyledUserDetailComment>
-        {data.detailComment?.split('\n').map(comment => (
-          <>
-            {comment}
-            <br />
-          </>
-        ))}
-      </StyledUserDetailComment>
-      <StyledMobileModifyButton>수정하기</StyledMobileModifyButton>
-      <StyledCourseContainer>
-        <StyledCourseTab>
-          <StyledTab onClick={() => {setCourseTab('now'); setCourseSemester(courseNow);}}>
-            <StyledTabText active={courseTab === 'now'}>현재 활동</StyledTabText>
-            {courseTab === 'now' ? <StyledTabLine /> : ''}
-          </StyledTab>
-          |
-          <StyledTab onClick={() => {setCourseTab('past'); setCourseSemester(coursePast);}}>
-            <StyledTabText active={courseTab === 'past'}>지난 활동</StyledTabText>
-            {courseTab === 'past' ? <StyledTabLine /> : ''}
-          </StyledTab>
-        </StyledCourseTab>
-        <StyledLine />
-        <StyledMainCourseWrapper>
-          { courseSemester?.map((course: Course, i: number) => (
-            <StyledMainCourse
-              key={i}
+        <StyledUserInfoContainer>
+          <StyledUserEmoji>{data.emoji}</StyledUserEmoji>
+          <StyledUserContainer>
+            <StyledName>
+              {data.name}
+              <StyledUserRole>{data.role}</StyledUserRole>
+            </StyledName>
+            <StyledComment>{data.comment}</StyledComment>
+            <StyledSocialContainer>
+              <StyledSocialBox>
+                <img src={`${process.env.PUBLIC_URL}/img/social/email.svg`} />
+                <StyledSocialLink href={email} target='_blank'>
+                  {data.email}
+                </StyledSocialLink>
+              </StyledSocialBox>
+              <StyledSocialBox>
+                <img src={`${process.env.PUBLIC_URL}/img/social/github.svg`} />
+                <StyledSocialLink href={data.link} target='_blank'>
+                  {/* {data.link} */}
+                  {github_id}
+                </StyledSocialLink>
+              </StyledSocialBox>
+              <StyledSocialBox>
+                <img src={`${process.env.PUBLIC_URL}/img/social/instagram.svg`} />
+                <StyledSocialLink href={instagram} target='_blank'>
+                  @{data.instaLink}
+                </StyledSocialLink>
+              </StyledSocialBox>
+            </StyledSocialContainer>
+          </StyledUserContainer>
+          <StyledPcModifyButton>수정하기</StyledPcModifyButton>
+        </StyledUserInfoContainer>
+        <StyledUserDetailComment>
+          {data.detailComment?.split('\n').map(comment => (
+            <>
+              {comment}
+              <br />
+            </>
+          ))}
+        </StyledUserDetailComment>
+        <StyledMobileModifyButton>수정하기</StyledMobileModifyButton>
+        <StyledCourseContainer>
+          <StyledCourseTab>
+            <StyledTab
               onClick={() => {
-                history.push(`/course/detail/${course.id}`);
+                setCourseTab('now');
               }}>
-              <StyledCourseProfile>
-                <StyledCourseProfileImg>
-                  <StyledCourseProfileCircle />
-                  <StyledCourseProfileEmoji>{course.courseLeader.emoji}</StyledCourseProfileEmoji>
-                </StyledCourseProfileImg>
-                <StyledCourseProfileName>
-                  {course.courseLeader.name}&nbsp;
-                  <span>팀장</span>
-                </StyledCourseProfileName>
-              </StyledCourseProfile>
+              <StyledTabText active={courseTab === 'now'}>현재 활동</StyledTabText>
+              {courseTab === 'now' ? <StyledTabLine /> : ''}
+            </StyledTab>
+            |
+            <StyledTab
+              onClick={() => {
+                setCourseTab('past');
+              }}>
+              <StyledTabText active={courseTab === 'past'}>지난 활동</StyledTabText>
+              {courseTab === 'past' ? <StyledTabLine /> : ''}
+            </StyledTab>
+          </StyledCourseTab>
+          <StyledLine />
+          <StyledMainCourseWrapper>
+            {courseSemester?.map((course: Course, i: number) => (
+              <StyledMainCourse
+                key={i}
+                onClick={() => {
+                  history.push(`/course/detail/${course.id}`);
+                }}>
+                <StyledCourseProfile>
+                  <StyledCourseProfileImg>
+                    <StyledCourseProfileCircle />
+                    <StyledCourseProfileEmoji>{course.courseLeader.emoji}</StyledCourseProfileEmoji>
+                  </StyledCourseProfileImg>
+                  <StyledCourseProfileName>
+                    {course.courseLeader.name}&nbsp;
+                    <span>팀장</span>
+                  </StyledCourseProfileName>
+                </StyledCourseProfile>
 
-              <StyledCourseInfo>
-                <StyledStackTitle>
-                  <StyledStackWrapper>
-                    {course.language.map((a: string, i: number) => (
-                      <StyledStackImg key={i} src={`${process.env.PUBLIC_URL}/img/icon/${a}.svg`} />
+                <StyledCourseInfo>
+                  <StyledStackTitle>
+                    <StyledStackWrapper>
+                      {course.language.map((a: string, i: number) => (
+                        <StyledStackImg
+                          key={i}
+                          src={`${process.env.PUBLIC_URL}/img/icon/${a}.svg`}
+                        />
                       ))}
-                  </StyledStackWrapper>
-                  <StyledCourseTitle>{course.courseName}</StyledCourseTitle>
-                </StyledStackTitle>
+                    </StyledStackWrapper>
+                    <StyledCourseTitle>{course.courseName}</StyledCourseTitle>
+                  </StyledStackTitle>
                   <StyledCourseDetail>
                     난이도:&nbsp;<StyledCourseDetail2>{course.difficulty}</StyledCourseDetail2>
                     &ensp;/&ensp;투자시간:&nbsp;
                     <StyledCourseDetail2>{course.requireTime}학점</StyledCourseDetail2>
                   </StyledCourseDetail>
-              </StyledCourseInfo>
-              {courseTab === 'now' ? 
-                <StyledRegisterButton
+                </StyledCourseInfo>
+                {courseTab === 'now' ? (
+                  <StyledRegisterButton
                   // onClick={() => {
                   //   courseCancelButton(i);
                   // }}
                   >
-                  수강 취소
-                </StyledRegisterButton>
-              : ''}
-            </StyledMainCourse>
-          ))}
-        </StyledMainCourseWrapper>
-        
-      </StyledCourseContainer>
+                    수강 취소
+                  </StyledRegisterButton>
+                ) : (
+                  ''
+                )}
+              </StyledMainCourse>
+            ))}
+          </StyledMainCourseWrapper>
+        </StyledCourseContainer>
       </StyledCommonPcLayout>
     </StyledCommonLayout>
   );
