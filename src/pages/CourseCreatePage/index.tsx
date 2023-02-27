ef451dd02c88e6db947c8e8081342994a76a05bb
yuna Kim <yuna000804@gmail.com>
2023-02-27 17:01:00 +0900

    FEAT : 강의개설페이지 입력값 상태 생성

--- src/pages/CourseCreatePage/index.tsx
+++ src/pages/CourseCreatePage/index.tsx
@@ -5,9 +5,6 @@ import { useRecoilState } from 'recoil';
 
 import { LanguageList } from '@constants';
 import {
-  courseDifficultyState,
-  courseRequireTimeState,
-  courseTypeState,
   selectedLanguagesState,
 } from '@recoil';
 import { StyledDownArrow } from '@utility/COMMON_STYLE';
@@ -15,8 +12,8 @@ import { StyledDownArrow } from '@utility/COMMON_STYLE';
 import {
   StyledBodyBox,
   StyledBox,
+  StyledCategoryButton,
   StyledComment,
-  StyledDifficultyButton,
   StyledDropDown,
   StyledDropDownWrapper,
   StyledHorizontalLine,
@@ -29,18 +26,13 @@ import {
   StyledMemberInput,
   StyledMenu,
   StyledPlaceholder,
-  StyledRequireTimeButton,
   StyledSelect,
   StyledSubTitle,
   StyledTitle,
   StyledTitleBox,
-  StyledTypeButton,
 } from './style';
 
 export const CourseCreatePage = () => {
-  const [courseType, setCourseType] = useRecoilState(courseTypeState);
-  const [courseDifficulty, setCourseDifficulty] = useRecoilState(courseDifficultyState);
-  const [courseRequireTime, setCourseRequireTime] = useRecoilState(courseRequireTimeState);
   const [selectedLanguages, setSelectedLanguages] = useRecoilState(selectedLanguagesState);
   const [mainLanguageImg, setMainLanguageImg] = useState('Etc');
 
@@ -55,64 +47,120 @@ export const CourseCreatePage = () => {
     onMainLanguageImg();
   }, [selectedLanguages]);
 
+  // 필수정보
   const course = {
     type: ['세션', '스터디', '프로젝트'],
     difficulty: ['easy', 'medium', 'hard'],
     requireTime: ['1학점', '2학점', '3학점'],
   };
 
-  const [inputs, setInputs] = useState({
-    '1주차': '',
-    '2주차': '',
-    '3주차': '',
-    '4주차': '',
-    '5주차': '',
-    '6주차': '',
-    '7주차': '',
-    '8주차': '',
+  const [requireInform, setRequireInform] = useState<any>({
+    courseType: '',
+    difficulty: '',
+    requireTime: '',
+    courseName: '',
   });
 
-  const handleCurri = (e: any) => {
-    console.log(e);
-    // const value = e.target;
+  const onChangeRequireInform = (type: string, value: string | string[]) => {
+    const result = { ...requireInform };
+    result[type] = value;
+    setRequireInform(result);
   };
 
+  console.log(requireInform, selectedLanguages);
+
+  // 세부정보
+  const [detailInform, setDetailInform] = useState<any>({
+    courseStack: [],
+    courseInfo: '',
+    courseGoal: '',
+    maxMemberNum: '',
+    courseDate: '',
+    coursePlace: '',
+    courseAdmin: '',
+  });
+  console.log(detailInform);
+
+  const onChangeDetailInform = (type: string, value: string | string[]) => {
+    const result = { ...detailInform };
+    result[type] = value;
+    setDetailInform(result);
+  };
+
+  // 커리큘럼
+  const [curriInform, setCurriInform] = useState<{ [key: string]: string }>({
+    week1: '',
+    week2: '',
+    week3: '',
+    week4: '',
+    week5: '',
+    week6: '',
+    week7: '',
+    week8: '',
+  });
+
+  console.log(curriInform);
   const TypeMenu = (
-    <Menu>
-      {course.type.map((type, key) => {
-        return (
-          <Menu.Item key={key}>
-            <a onClick={() => setCourseType(type)}>{type}</a>
-          </Menu.Item>
-        );
-      })}
-    </Menu>
+    <Menu
+      onClick={e => {
+        onChangeRequireInform('courseType', e.key);
+      }}
+      selectedKeys={requireInform['courseType']}
+      items={course.type.map(type => ({
+        label: type,
+        key: type,
+      }))}
+    />
   );
 
   const DifficultyMenu = (
-    <Menu>
-      {course.difficulty.map((difficulty, key) => {
-        return (
-          <Menu.Item key={key}>
-            <a onClick={() => setCourseDifficulty(difficulty)}>{difficulty}</a>
-          </Menu.Item>
-        );
-      })}
-    </Menu>
+    <Menu
+      onClick={e => {
+        onChangeRequireInform('difficulty', e.key);
+      }}
+      selectedKeys={[requireInform['difficulty']]}
+      items={course.difficulty.map(difficulty => ({
+        label: difficulty,
+        key: difficulty,
+      }))}
+    />
   );
 
   const RequireTimeMenu = (
-    <Menu>
-      {course.requireTime.map((time, key) => {
-        return (
-          <Menu.Item key={key}>
-            <a onClick={() => setCourseRequireTime(time)}>{time}</a>
-          </Menu.Item>
-        );
-      })}
-    </Menu>
+    <Menu
+      onClick={e => {
+        onChangeRequireInform('requireTime', e.key);
+      }}
+      selectedKeys={[requireInform['requireTime']]}
+      items={course.requireTime.map(time => ({
+        label: time,
+        key: time,
+      }))}
+    />
   );
 
+  const courseCategoryData = [
+    {
+      name: '세션 / 스터디 / 프로젝트',
+      type: 'type',
+      menu: TypeMenu,
+      defaultValue: 'courseType',
+    },
+    {
+      name: '난이도',
+      type: 'difficulty',
+      menu: DifficultyMenu,
+      defaultValue: 'difficulty',
+    },
+    {
+      name: '투자시간',
+      type: 'requireTime',
+      menu: RequireTimeMenu,
+      defaultValue: 'requireTime',
+    },
+  ];
+
+  // 주요 스택
   const handleSelectedLanguages = (stack: Language) => {
     if ([...selectedLanguages].includes(stack)) {
       const stacks = [...selectedLanguages].filter(res => res !== stack);
@@ -120,6 +168,7 @@ export const CourseCreatePage = () => {
     } else {
       setSelectedLanguages([...selectedLanguages, stack]);
     }
+    onChangeRequireInform('language', selectedLanguages);
   };
 
   const LanguageMenu = (
@@ -127,7 +176,6 @@ export const CourseCreatePage = () => {
       {LanguageList.map(res => {
         return (
           <Menu.Item key={res}>
-            {/* <a onClick={() => setSelectedLanguages(res)}>{res}</a> */}
             <Checkbox
               onClick={() => {
                 handleSelectedLanguages(res);
@@ -140,6 +188,7 @@ export const CourseCreatePage = () => {
     </Menu>
   );
 
+  // 세부 스택
   const options: SelectProps['options'] = [];
   const courseStack = ['Git', 'NextJS', 'Express', 'Typescript', 'Pandas', 'Pytorch', 'Numpy'];
 
@@ -149,11 +198,6 @@ export const CourseCreatePage = () => {
       label: stack,
     });
   });
-
-  const handleStackChange = (value: string) => {
-    console.log(`selected ${value}`);
-    // 세부 기술 스택 넣어놓는 곳
-  };
 
   return (
     <>
@@ -171,39 +215,25 @@ export const CourseCreatePage = () => {
           <StyledBodyBox>
             <div>
               <StyledSubTitle>활동 카테고리</StyledSubTitle>
-
               <StyledDropDownWrapper>
-                <StyledDropDown>
-                  <Dropdown overlay={TypeMenu} placement='bottomLeft'>
-                    <StyledTypeButton>
-                      <StyledDownArrow width='5' />
-                      {courseType}
-                    </StyledTypeButton>
-                  </Dropdown>
-                </StyledDropDown>
-
-                <StyledDropDown>
-                  <Dropdown overlay={DifficultyMenu} placement='bottomLeft'>
-                    <StyledDifficultyButton>
-                      <StyledDownArrow width='5' />
-                      {courseDifficulty}
-                    </StyledDifficultyButton>
-                  </Dropdown>
-                </StyledDropDown>
-
-                <StyledDropDown>
-                  <Dropdown overlay={RequireTimeMenu} placement='bottomLeft'>
-                    <StyledRequireTimeButton>
-                      <StyledDownArrow width='5' />
-                      {courseRequireTime}
-                    </StyledRequireTimeButton>
-                  </Dropdown>
-                </StyledDropDown>
+                {courseCategoryData.map((category, key) => (
+                  <StyledDropDown key={key}>
+                    <Dropdown overlay={category.menu} placement='bottomLeft'>
+                      <StyledCategoryButton>
+                        <StyledDownArrow width='5' />
+                        {requireInform[category.defaultValue] || category.name}
+                      </StyledCategoryButton>
+                    </Dropdown>
+                  </StyledDropDown>
+                ))}
               </StyledDropDownWrapper>
             </div>
             <div>
               <StyledSubTitle>활동 제목</StyledSubTitle>
-              <StyledInput placeholder='30자 이내로 작성해주세요.' />
+              <StyledInput
+                onChange={e => onChangeRequireInform('courseName', e.target.value)}
+                placeholder='30자 이내로 작성해주세요.'
+              />
             </div>
 
             <div>
@@ -217,17 +247,17 @@ export const CourseCreatePage = () => {
                 <StyledLanguageImg
                   src={`${process.env.PUBLIC_URL}/img/icon/${mainLanguageImg}.svg`}
                 />
+
                 <Dropdown
-                  // dropdownRender={menu => <div style={{ height: 500, width: 200 }}>{menu}</div>}
                   trigger={['click']}
                   overlay={LanguageMenu}>
                   <StyledLanguage>
                     {selectedLanguages.length > 0 ? (
+                      // selectedLanguages.join(', ')
                       selectedLanguages.map((img, i) => (
                         <img key={i} src={`${process.env.PUBLIC_URL}/img/icon/${img}.svg`} />
                       ))
                     ) : (
-                      // selectedLanguages.join(', ')
                       <>
                         <StyledPlaceholder>옵션을 선택해주세요.</StyledPlaceholder>
                       </>
@@ -257,7 +287,9 @@ export const CourseCreatePage = () => {
                 <Select
                   mode='tags'
                   style={{ width: '100%' }}
-                  onChange={handleStackChange}
+                  onChange={e => {
+                    onChangeDetailInform('courseStack', e);
+                  }}
                   tokenSeparators={[',']}
                   options={options}
                   placeholder='200자 이내로 옵션을 선택해주세요.'
@@ -269,7 +301,7 @@ export const CourseCreatePage = () => {
               <StyledSubTitle>활동 소개</StyledSubTitle>
               <StyledInput2
                 onChange={e => {
-                  console.log(e.target.value);
+                  onChangeDetailInform('courseInfo', e.target.value);
                 }}
                 placeholder='200자 이내로 작성해주세요.'
               />
@@ -277,13 +309,21 @@ export const CourseCreatePage = () => {
 
             <div>
               <StyledSubTitle>활동 목표</StyledSubTitle>
-              <StyledInput2 placeholder='200자 이내로 작성해주세요.' />
+              <StyledInput2
+                onChange={e => {
+                  onChangeDetailInform('courseGoal', e.target.value);
+                }}
+                placeholder='200자 이내로 작성해주세요.'
+              />
             </div>
 
-            {/* 인원 */}
             <div>
               <StyledSubTitle>활동 인원</StyledSubTitle>
-              <StyledMemberInput placeholder='활동 인원' />
+              <StyledMemberInput
+                onChange={e => onChangeDetailInform('maxMemberNum', e.target.value)}
+                type='number'
+                placeholder='활동 인원'
+              />
             </div>
             <div>
               <StyledTitleBox style={{ margin: '0px' }}>
@@ -292,7 +332,10 @@ export const CourseCreatePage = () => {
                   Tip. 아직 정해지지 않은 경우, &apos;미정&apos;이라고 작성해주세요.
                 </StyledComment>
               </StyledTitleBox>
-              <StyledInput placeholder='100자 이내로 작성해주세요.' />
+              <StyledInput
+                onChange={e => onChangeDetailInform('courseDate', e.target.value)}
+                placeholder='100자 이내로 작성해주세요.'
+              />
             </div>
 
             <div>
@@ -302,12 +345,18 @@ export const CourseCreatePage = () => {
                   Tip. 동방에서 진행하실 경우, 시간표를 별도로 등록해주세요!
                 </StyledComment>
               </StyledTitleBox>
-              <StyledInput2 placeholder='200자 이내로 작성해주세요.' />
+              <StyledInput2
+                onChange={e => onChangeDetailInform('coursePlace', e.target.value)}
+                placeholder='200자 이내로 작성해주세요.'
+              />
             </div>
 
             <div>
               <StyledSubTitle>유의사항</StyledSubTitle>
-              <StyledInput2 placeholder='200자 이내로 작성해주세요.' />
+              <StyledInput2
+                onChange={e => onChangeDetailInform('courseAdmin', e.target.value)}
+                placeholder='200자 이내로 작성해주세요.'
+              />
             </div>
           </StyledBodyBox>
         </StyledBox>
@@ -321,10 +370,17 @@ export const CourseCreatePage = () => {
           </StyledTitleBox>
           <StyledHorizontalLine />
           <StyledBodyBox>
-            {Object.keys(inputs).map((week, i) => (
+            {Object.keys(curriInform).map((week, i) => (
               <div key={i}>
-                <StyledSubTitle>{week}</StyledSubTitle>
-                <StyledInput2 onChange={handleCurri} placeholder='200자 이내로 작성해주세요.' />
+                <StyledSubTitle>{i + 1}주차</StyledSubTitle>
+                <StyledInput2
+                  onChange={e => {
+                    const result = { ...curriInform };
+                    result[week] = e.target.value;
+                    setCurriInform(result);
+                  }}
+                  placeholder='200자 이내로 작성해주세요.'
+                />
               </div>
             ))}
           </StyledBodyBox>
