c8ca2d070876b7cd6eba0690684ad9561c391cbc
yuna Kim <yuna000804@gmail.com>
2023-03-06 13:05:38 +0900

    STYLE : 홈 화면 사용언어 탭 스크롤 추가

--- src/pages/MainPage/MainSearch/index.tsx
+++ src/pages/MainPage/MainSearch/index.tsx
@@ -64,6 +64,7 @@ export const MainSearch = () => {
       </StyledSearchContainer>
       <StyledDropDown>
         <Dropdown
+          overlayStyle={{ overflowY: 'scroll' }}
           dropdownRender={menu => <div style={{ height: 500, width: 200 }}>{menu}</div>}
           trigger={['click']}
           overlay={LanguageMenu}>
