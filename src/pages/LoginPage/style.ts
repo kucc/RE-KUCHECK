02ecd3d35156e7bb08371fe318a3518365f4a26a
yuna Kim <yuna000804@gmail.com>
2023-03-08 14:37:36 +0900

    STYLE : 로그인 페이지 배경 길이 및 색상 수정

--- src/pages/LoginPage/style.ts
+++ src/pages/LoginPage/style.ts
@@ -1,11 +1,8 @@
 import styled from 'styled-components';
 
-import { BASE_COLOR } from '@utility/COLORS';
-
 export const StyledCenterContainer = styled.main`
   display: flex;
   justify-content: center;
-  background-color: ${BASE_COLOR};
   position: relative;
 `;
 
@@ -13,7 +10,6 @@ export const StyledAuthContainer = styled.div<{ isLogin: boolean }>`
   width: 100%;
   max-width: 600px;
   margin: 0 auto;
-  min-height: 700px;
+  min-height: 850px;
   height: ${props => (props.isLogin ? 'calc(100vh - 54px)' : 'auto')};
-  background-color: white;
 `;
