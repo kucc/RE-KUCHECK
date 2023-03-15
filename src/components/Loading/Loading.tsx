f3708a2f26c57ef6161bbdcce784bf3e0a851922
LHJ <jjs01hwang@gmail.com>
2023-03-16 02:27:50 +0900

    FIX : 로딩 추가

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,4 +1,8 @@
+import Lottie from 'lottie-react';
+
 import { Portal } from '@components';
+
+import loadingAnimation from '../../assets/lottie/loading.json';
 
 export const Loading = () => {
   return (
@@ -9,9 +13,10 @@ export const Loading = () => {
           height: '100%',
           width: '100%',
           flexDirection: 'column',
+          justifyItems: 'center',
           alignItems: 'center',
         }}>
-        {/* <Lottie animationData={loadingAnimation} loop={true} style={{ height: 700, width: 700 }} /> */}
+        <Lottie animationData={loadingAnimation} loop={true} style={{ height: 500, width: 500 }} />
       </div>
     </Portal>
   );
