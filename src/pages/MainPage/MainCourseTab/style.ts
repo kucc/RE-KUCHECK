a77977bc4d3315ec62e86812f950ef02e105721f
LHJ <jjs01hwang@gmail.com>
2023-03-16 23:15:33 +0900

    FIX : layout fix

--- src/pages/MainPage/MainCourseTab/style.ts
+++ src/pages/MainPage/MainCourseTab/style.ts
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 export const StyledCourseContainer = styled.div`
   min-height: 700px;
   margin-bottom: 92px;
+  width: 1100px;
   @media (max-width: 800px) {
+    width: auto;
     margin-bottom: 52px;
   }
 `;
